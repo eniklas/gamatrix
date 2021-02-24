2f6ee64951d45b1fc4e0e733c67d8ba06c70cc43
Erik Niklas <erik@erikniklas.net>
2021-02-23 22:24:59 -0800

    update with D$'s test

--- helpers/cache_helper.py
+++ helpers/cache_helper.py
@@ -3,26 +3,43 @@ import logging
 import os
 
 
-class Cache:
+class Cache(dict):
     def __init__(self, cache_file):
         self.cache_file = cache_file
-        self.data: CacheDict = CacheDict()
-        self.data.dirty: bool = False
+        self.data = {}
+        self.dirty = False
         self.log = logging.getLogger(__name__)
-        self.log.info(f"Debug: self.data.dirty = {self.data.dirty}")
 
         if os.path.exists(self.cache_file):
+            self.log.debug(f"Reading cache from file {self.cache_file}")
             with open(self.cache_file, "r") as f:
                 self.data = json.load(f)
+        else:
+            self.log.warning(
+                f"Cache file {self.cache_file} not found, making new cache"
+            )
+        super().__init__()
+
+    def __setitem__(self, k, v):
+        self.dirty = True
+        super().__setitem__(k, v)
+
+    def __delitem__(self, k):
+        self.dirty = True
+        super().__delitem__(k)
 
     def save(self):
-        if self.data.dirty == True:
+        if self.dirty:
             self.log.info("DEBUG: cache is dirty, saving")
             with open(self.cache_file, "w") as f:
                 json.dump(self.data, f)
 
-            self.data.dirty = False
+            self.dirty = False
+        else:
+            self.log.info("DEBUG: cache is clean, not saving")
 
+
+"""
 
 # Use this for the cache so we can mark it dirty on any update
 class CacheDict(dict):
@@ -36,3 +53,29 @@ class CacheDict(dict):
     def __delitem__(self, v) -> None:
         self.dirty = True
         return super().__delitem__(v)
+
+
+class Cache(dict):
+    def __init__(self):
+        self.dd = False
+        super().__init__()
+
+    def __setitem__(self, k, v):
+        self.dd = True
+        super().__setitem__(k, v)
+
+    def __delitem__(self, key):
+        self.dd = True
+        super().__delitem__(key)
+
+    def is_dirty(self):
+        return self.dd
+
+    def self_save(self):
+        if self.dd:
+            print(self)
+        else:
+            print("NAH")
+        if self.dd:
+            self.dd = False
+"""
