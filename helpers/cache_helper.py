4ffcf541e3d5300e9654304acc4d06885334c652
Erik Niklas <erik@erikniklas.net>
2021-02-23 21:00:25 -0800

    POS

--- helpers/cache_helper.py
+++ helpers/cache_helper.py
@@ -7,7 +7,7 @@ class Cache:
     def __init__(self, cache_file):
         self.cache_file = cache_file
         self.data: CacheDict = CacheDict()
-        self.data.dirty = False
+        self.data.dirty: bool = False
         self.log = logging.getLogger(__name__)
         self.log.info(f"Debug: self.data.dirty = {self.data.dirty}")
 
@@ -16,7 +16,7 @@ class Cache:
                 self.data = json.load(f)
 
     def save(self):
-        if self.data.dirty:
+        if self.data.dirty == True:
             self.log.info("DEBUG: cache is dirty, saving")
             with open(self.cache_file, "w") as f:
                 json.dump(self.data, f)
@@ -29,6 +29,10 @@ class CacheDict(dict):
     def __init__(self):
         self.dirty = False
 
-    def __setitem__(self, k, v):
+    def __setitem__(self, k, v) -> None:
         self.dirty = True
         super(CacheDict, self).__setitem__(k, v)
+
+    def __delitem__(self, v) -> None:
+        self.dirty = True
+        return super().__delitem__(v)
