c9c6ff72db75f6729f10435894f1ff3193b0b706
Erik Niklas <github@bobanddoug.com>
2021-02-24 20:46:51 -0800

    add simple dirty cache mechanism (#46)
    * add simple dirty cache mechanism
    
    * save cache when dirty; remove main from igdb_helper; add info logging
    
    * remove redundant thing
    
    * warn when making new cache file; remove unused import
    
    Co-authored-by: Erik Niklas <erik@erikniklas.net>

--- helpers/cache_helper.py
+++ helpers/cache_helper.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 
 
@@ -6,11 +7,24 @@ class Cache:
     def __init__(self, cache_file):
         self.cache_file = cache_file
         self.data = {}
+        self.log = logging.getLogger(__name__)
 
         if os.path.exists(self.cache_file):
+            self.log.debug(f"Reading cache file {self.cache_file}")
             with open(self.cache_file, "r") as f:
                 self.data = json.load(f)
 
+        else:
+            self.log.warning(
+                f"Cache file {self.cache_file} not found, making new cache"
+            )
+
+        self.data["dirty"] = False
+
     def save(self):
-        with open(self.cache_file, "w") as f:
-            json.dump(self.data, f)
+        if self.data["dirty"]:
+            self.log.debug("Cache is dirty, saving")
+            with open(self.cache_file, "w") as f:
+                json.dump(self.data, f)
+        else:
+            self.log.debug("Cache is clean, not saving")
